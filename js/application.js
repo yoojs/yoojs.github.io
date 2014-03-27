e306e27926d2b071cdbe68f733a24c1f1bb8e16a
yoojs <yoojs@dukes.jmu.edu>
2014-03-27 15:46:14 -0400

    intial commit

--- js/application.js
+++ js/application.js
@@ -5,18 +5,22 @@ window.requestAnimationFrame(function () {
 
 
     var imageList = [
-        "img/dan212-2.png",
-        "img/dan212-4.png",
-        "img/dan212-8.png",
-        "img/dan212-16.png",
-        "img/dan212-32.png",
-        "img/dan212-64.png",
-        "img/dan212-128.png",
-        "img/dan212-256.png","img/dan212-512.png","img/dan212-1024.png",
-        "img/dan212-2048.png"
+        "https://raw.githubusercontent.com/yoojs/yoojs.github.io/master/img/dan212-2.png",
+        "https://raw.githubusercontent.com/yoojs/yoojs.github.io/master/img/dan212-4.png",
+        "https://raw.githubusercontent.com/yoojs/yoojs.github.io/master/img/dan212-8.png",
+        "https://raw.githubusercontent.com/yoojs/yoojs.github.io/master/img/dan212-16.png",
+        "https://raw.githubusercontent.com/yoojs/yoojs.github.io/master/img/dan212-32.png",
+        "https://raw.githubusercontent.com/yoojs/yoojs.github.io/master/img/dan212-64.png",
+        "https://raw.githubusercontent.com/yoojs/yoojs.github.io/master/img/dan212-128.png",
+        "https://raw.githubusercontent.com/yoojs/yoojs.github.io/master/img/dan212-256.png",
+        "https://raw.githubusercontent.com/yoojs/yoojs.github.io/master/img/dan212-512.png",
+        "https://raw.githubusercontent.com/yoojs/yoojs.github.io/master/img/dan212-1024.png",
+        "https://raw.githubusercontent.com/yoojs/yoojs.github.io/master/img/dan212-2048.png",
+        
     ];
     for(var i = 0; i < imageList.length; i++ ) 
     {
         var imageObject = new Image();
         imageObject.src = imageList[i];
     }
+
